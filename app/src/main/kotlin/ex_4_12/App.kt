7d2576ca92a95ad7587d48da2e9e0fa33f62dc1a
Denis Manikhin <dmanikhine@gmail.com>
2021-11-07 17:18:47 +0100

    Final commit

--- app/src/main/kotlin/ex_4_12/App.kt
+++ app/src/main/kotlin/ex_4_12/App.kt
@@ -3,13 +3,52 @@
  */
 package ex_4_12
 
+
+fun <T> List<T>.head(): T =
+        if (this.isEmpty())
+            throw IllegalArgumentException("head called on empty list")
+        else
+            this[0]
+
+fun <T> List<T>.tail(): List<T> =
+        if (this.isEmpty())
+            throw IllegalArgumentException("tail called on empty list")
+        else
+            this.drop(1)
+
+
+fun <T, U> foldLeft(list: List<T>, z: U, f: (U, T) -> U): U {
+    tailrec fun foldLeft(list: List<T>, acc: U): U =
+            if (list.isEmpty())
+                acc
+            else
+                foldLeft(list.tail(), f(acc, list.head()))
+    return foldLeft(list, z)
+}
+
+
+fun <T> prepend(elem: T, list: List<T>): List<T> = foldLeft(list, listOf(elem)) { lst, elm -> lst + elm }
+
+
+fun range(start: Int, end: Int): List<Int> =
+    if (end < start)
+        listOf()
+    else
+        prepend(start,range(start + 1, end))
+		
+		
+
+
+
 class App {
     val greeting: String
         get() {
-            return "Hello World!"
+            return "package ex_4_12"
         }
 }
 
 fun main() {
     println(App().greeting)
+	println(range(1,10))
+	
 }
